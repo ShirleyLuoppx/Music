edaf2a2c723c0547b99e614b195ea494a72bfd9f
luoxia <1184793244@qq.com>
2024-07-17 09:35:25 +0800

    update viewbinding to databinding

--- app/src/main/java/com/ppx/music/MainActivity.kt
+++ app/src/main/java/com/ppx/music/MainActivity.kt
@@ -4,7 +4,6 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
 import android.widget.EditText
-import androidx.core.widget.addTextChangedListener
 import com.ppx.music.databinding.ActivityMainBinding
 import com.ppx.music.common.ApiConstants
 import com.ppx.music.utils.LogUtils
@@ -16,11 +15,11 @@ class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
 
         binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
-        findViewById<Button>(R.id.btn_verify_code).setOnClickListener {
+        binding.btnVerifyCode.setOnClickListener {
             LogUtils.d("click btn.......")
 
             val phoneNumber = binding.etPhoneNumber.text.toString()
@@ -31,7 +30,7 @@ class MainActivity : AppCompatActivity() {
         val etVerifyCode = findViewById<EditText>(R.id.et_verify_code)
 
 
-        findViewById<Button>(R.id.btn_login).setOnClickListener {
+        binding.btnLogin.setOnClickListener {
             val phoneNumber = binding.etPhoneNumber.text.toString()
             LogUtils.d("click btnLogin and phoneNumber = $phoneNumber")
             val verifyCode = etVerifyCode.text.toString()
